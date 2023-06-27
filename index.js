c08296d848f406622c421da4387829504a44019a
AnthonyGlt <agullient@cirilgroup.com>
2023-06-27 17:21:47 +0200

    cleaned work

---
+++ index.js
@@ -0,0 +1,37 @@
+var port = process.env.PORT || 8070;
+
+
+var express = require('express');
+const { dirname } = require('path');
+var app = express();
+var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/'));
+
+
+http.listen(port, function () {
+});
+
+
+
+
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+
+
+  socket.on('switchScene', function (msg) {
+    console.log('switchScene: ' + msg);
+    socket.broadcast.emit('switchScene', msg);  //to do comment
+    //io.emit('changeBDD', msg);
+  });
+
+
+
+});
+
